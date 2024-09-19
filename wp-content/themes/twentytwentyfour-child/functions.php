22a978ebe0edc21397b5d798052e2275ba146bef
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-09-19 23:04:56 +0200

    custom type

--- wp-content/themes/twentytwentyfour-child/functions.php
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -5,3 +5,24 @@ function add_css()
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'add_css');
+
+add_action('init', function(){
+    register_post_type('book',[
+        'label' => __('Books', 'txtdomain'),
+        'public'=> true,
+        'menu_position'=> 5,
+        'supports'=> ['title', 'editor', 'thumbnail', 'author', 'revisions', 'comments'],
+        'show_in_rest'=> true,
+        'rewrite'=> ['slug'=> 'book'],
+        'labels'=> [
+            'singular_name'=> __('Book', 'txtdomain'),
+            'add_new_item'=> __('Add new item', 'txtdomain'),
+            'new_item'=> __('New book', 'txtdomain'),
+            'view_item'=> __('View book', 'txtdomain'),
+            'not_found'=> __('No books found', 'txtdomain'),
+            'not_found' => __('No books found in trash', 'txtdomain'),
+            'all_items'=> __('All books', 'txtdomain'),
+            'insert_into_item'=> __('Insert into book', 'txtdomain'),
+        ]
+    ])
+})
